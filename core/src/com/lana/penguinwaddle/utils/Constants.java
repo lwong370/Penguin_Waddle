79de1f8d595112889c8b3093a3ab5077fc355488
Lana <lanawo370@gmail.com>
2020-08-06 16:14:31 -0700

    Fix penguin size and linear impulse

--- core/src/com/lana/penguinwaddle/utils/Constants.java
+++ core/src/com/lana/penguinwaddle/utils/Constants.java
@@ -17,12 +17,12 @@ public class Constants {
     public static final float RUNNER_Y = GROUND_HEIGHT;
     public static final float RUNNER_DODGE_X = 3;
     public static final float RUNNER_DODGE_Y = 3.4f;
-    public static final float RUNNER_WIDTH = 3.5f;
-    public static final float RUNNER_HEIGHT = 5.25f;
+    public static final float RUNNER_WIDTH = 3f;
+    public static final float RUNNER_HEIGHT = 4.5f;
     public static final float RUNNER_DENSITY = 0.5f;
 
     public static float RUNNER_GRAVITY_SCALE = 3f;
-    public static final Vector2 RUNNER_LINEAR_JUMP_IMPULSE = new Vector2(0, 150f);
+    public static final Vector2 RUNNER_LINEAR_JUMP_IMPULSE = new Vector2(0, 100f);
     public static final Vector2 RUNNER_LINEAR_STOP_TUMBLE_IMPULSE = new Vector2(-10, 0);
 
     public static final float OBSTACLE_X = 25f;
