2dfec6d265e0c56268e9778dff0c371f437c56c5
Lana <lanawo370@gmail.com>
2020-08-09 22:11:24 -0700

    Added fixture to Obstacle body representing rain.

--- core/src/com/lana/penguinwaddle/actors/Obstacle.java
+++ core/src/com/lana/penguinwaddle/actors/Obstacle.java
@@ -1,10 +1,11 @@
 package com.lana.penguinwaddle.actors;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.lana.penguinwaddle.box2d_physics.ObstacleUserData;
 import com.lana.penguinwaddle.enums.Difficulty;
 import com.lana.penguinwaddle.enums.GameState;
@@ -14,8 +15,6 @@ import com.lana.penguinwaddle.utils.GameManager;
 
 public class Obstacle extends GameActor {
 
-    private Animation animation;
-    private TextureRegion textureRegion;
     private float stateTime;
 
     public Obstacle(Body body) {
@@ -54,10 +53,17 @@ public class Obstacle extends GameActor {
                     rectangleRendered.y, rectangleRendered.width * 1.2f, rectangleRendered.height * 1.1f);
             if(getUserData().isStormRaining()){
                 batch.draw((TextureRegion) AssetsManager.getAnimation(Constants.OBSTACLE_RAIN_ASSETS_ID).getKeyFrame(stateTime, true), (rectangleRendered.x - (rectangleRendered.width * 0.03f)),
-                        rectangleRendered.y-100, rectangleRendered.width * 1.08f, rectangleRendered.height * 1.3f);
-                System.out.println("is rainingg");
+                        rectangleRendered.y-200, rectangleRendered.width * 1.08f, rectangleRendered.height * 2.5f);
+
+                //Add rain fixture
+                PolygonShape shape = new PolygonShape();
+                shape.setAsBox(6/2, 12/2);
+                body.createFixture(shape, Constants.OBSTACLE_DENSITY);
+                
             }else{
-                System.out.println("is not raining");
+                for(Fixture fixture: body.getFixtureList()){
+                    body.destroyFixture(fixture);
+                }
             }
         }
     }
